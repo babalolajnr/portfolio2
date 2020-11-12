21bc608ed31667afd83ee23a348d701ed3ee82fb
Abdulqudduus Babalola <babalolajnr@gmail.com>
2020-11-12 06:16:35 +0100

    added netlify identity

--- pages/index.vue
+++ pages/index.vue
@@ -161,4 +161,11 @@
   </div>
 </template>
 <script>
+export default {
+  head () {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    }
+  }
+}
 </script>
