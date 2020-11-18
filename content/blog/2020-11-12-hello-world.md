4dedefe38d3cd3f7d721c3dda5805024bfae2df5
babalolajnr <babalolajnr@gmail.com>
2020-11-18 22:04:07 +0100

    new computadora

--- content/blog/2020-11-12-hello-world.md
+++
@@ -1,6 +0,0 @@
----
-title: Hello World
-date: 2020-11-12T05:26:58.238Z
-description: Nice One
----
-Lorem ipsum
--- plugins/axios.js
+++
@@ -1,7 +0,0 @@
-export default function ({ $axios, redirect }) {
-  $axios.onError((error) => {
-    if (error.response.status === 500) {
-    //   redirect('/sorry')
-    }
-  })
-}
